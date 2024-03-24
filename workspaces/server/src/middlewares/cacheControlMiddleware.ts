ac2106e0966450fc6214281d501ad6c33787cf02
ym_ry_yoshida <ry_yoshida@yumemi.co.jp>
2024-03-24 14:03:48 +0900

    画像はブラウザ側でもキャッシュする

--- workspaces/server/src/middlewares/cacheControlMiddleware.ts
+++ workspaces/server/src/middlewares/cacheControlMiddleware.ts
@@ -2,6 +2,11 @@ import { createMiddleware } from 'hono/factory';
 
 export const cacheControlMiddleware = createMiddleware(async (c, next) => {
   await next();
-  c.res.headers.append('Cache-Control', 'private');
-  c.res.headers.append('Cache-Control', 'no-store');
+  if (c.req.url.includes('/images/') && c.req.method === 'GET') {
+    c.res.headers.append('Cache-Control', 'public');
+    c.res.headers.append('Cache-Control', 'max-age=3600');
+  } else {
+    c.res.headers.append('Cache-Control', 'private');
+    c.res.headers.append('Cache-Control', 'no-store');
+  }
 });
