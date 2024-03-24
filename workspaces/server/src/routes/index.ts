e85fa22faa8895b8584c6bd8243fd6bcbb3f7313
ym_ry_yoshida <ry_yoshida@yumemi.co.jp>
2024-03-24 13:54:24 +0900

    Revert "gzip圧縮に変える"
    This reverts commit 1aff1696dcf6d89306d73ad7188f6622936924a5.

--- workspaces/server/src/routes/index.ts
+++ workspaces/server/src/routes/index.ts
@@ -1,10 +1,10 @@
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
-import { compress } from 'hono/compress';
 import { HTTPException } from 'hono/http-exception';
 import { secureHeaders } from 'hono/secure-headers';
 
 import { cacheControlMiddleware } from '../middlewares/cacheControlMiddleware';
+import { compressMiddleware } from '../middlewares/compressMiddleware';
 
 import { adminApp } from './admin';
 import { apiApp } from './api';
@@ -24,7 +24,7 @@ app.use(
     origin: (origin) => origin,
   }),
 );
-app.use(compress());
+app.use(compressMiddleware);
 app.use(cacheControlMiddleware);
 
 app.get('/healthz', (c) => {
